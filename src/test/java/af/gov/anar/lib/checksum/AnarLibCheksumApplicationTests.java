55f5a7517901e0b40ea8f074b1582f41fe45ba4f
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 14:26:01 +0430

    check sum utility library

---
+++ src/test/java/af/gov/anar/lib/checksum/AnarLibCheksumApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.checksum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibCheksumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
